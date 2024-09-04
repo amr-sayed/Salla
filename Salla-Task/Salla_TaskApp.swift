3b6ed967dd2eb63fd95054643f057cf8deef960a
Amr Elsayed <amr.elsayed@fawry.com>
2024-09-04 22:29:40 +0300

    Initial Commit

---
+++ Salla-Task/Salla_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salla_TaskApp.swift
+//  Salla-Task
+//
+//  Created by Amr Ahmed on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Salla_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
